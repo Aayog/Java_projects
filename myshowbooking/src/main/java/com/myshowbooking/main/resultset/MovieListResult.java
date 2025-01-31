55715bf57b3851d707766964d7c1e91552d10a68
Aayog Koirala <koirala@miamioh.edu>
2025-01-31 07:52:15 -0500

    Updating clearning dir

--- myshowbooking/src/main/java/com/myshowbooking/main/resultset/MovieListResult.java
+++
@@ -1,9 +0,0 @@
-package com.myshowbooking.main.resultset;
-
-import java.util.List;
-
-public class MovieListResult<T> {
-	
-	private List<T> movielist;
-	private int total;
-}
--- myshowbooking/src/main/java/com/myshowbooking/main/userservice/dao/UserServiceDao.java
+++
@@ -1,9 +0,0 @@
-package com.myshowbooking.main.userservice.dao;
-
-import com.myshowbooking.main.user.model.User;
-
-public interface UserServiceDao {
-	
-	User findByUsername(String username);
-
-}
